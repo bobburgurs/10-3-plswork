560414f312a0c211d29872d059ba2d3650cdf975
bobburgurs <122063291+bobburgurs@users.noreply.github.com>
2023-10-03 16:27:49 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
+const express = require('express');
 
+const app = express();
+const port = 3000;
+
+app.use(express.static("./public"));
+app.get("/", function(req,res){
+res.sendFile(_dirname + "/src/pages/index.html");
+});
+
+app.post("/", function(req, res) {
+  res.redirect('/switch');
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log(`Server is running at on port ${process.env.PORT}.`);
+});
